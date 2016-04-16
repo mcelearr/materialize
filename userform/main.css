cba3e548576c04cbfeadb3562df0b402c41e3d5c
mcelearr <rorymcelearney@gmail.com>
2016-04-16 15:28:35 +0100

    add userform materialize example

---
+++ userform/main.css
@@ -0,0 +1,39 @@
+body{
+  overflow: hidden;
+  margin: 0;
+}
+.mask{
+  position: absolute;
+  z-index:200;
+  background-color: rgba(0,0,0,0.5);
+  width: 100%;
+  height: 100%;
+  display: none;
+}
+h3{
+  margin: 0px 20px 20px;
+}
+h5{
+  color: rgb(38, 166, 154)
+}
+form{
+  border-radius: 15px;
+  padding: 10px;
+}
+input[type=submit]{
+  float: right;
+}
+textarea{
+  border: 5px rgb(38, 166, 154);
+}
+.center {
+    font-size: 2em;
+    margin: auto;
+    width: 60%;
+    border: 3px solid rgb(38, 166, 154);
+    padding: 10px;
+    color: rgb(38, 166, 154);
+}
+.errorMessages{
+  color: red;
+}
